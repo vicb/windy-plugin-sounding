36c94e7d666aa03afe72754b5141f6322455dfaf
Victor Berchet <victor@suumit.com>
2019-03-14 12:57:29 -0700

    release 0.4.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,9 @@
 
 ## next
 
+## 0.4.1 - March 12 2019
+
 - Add support for changing timestamp via mouse wheel events.
 - Move inline styles to `plugin.less`.
+- Open the plugin to the left side.
 
--- src/config.js
+++ src/config.js
@@ -1,24 +1,10 @@
-/*
-	This is configuration file for your plugin
-*/
 module.exports = {
   displayName: "Better Sounding",
-
   hook: "contextmenu",
-
-  // External dependencies
   dependencies: [
     "https://cdn.jsdelivr.net/npm/d3@5/dist/d3.min.js",
     "https://cdn.jsdelivr.net/npm/preact@8/dist/preact.min.js",
   ],
-
-  // Class name applied to your plugin element
-  className: "drop-down-window ",
-  classNameMobile: "drop-down-window down", // for mobile devices
-
-  // The place in page, where your plugin element
-  // will be mounted to the page. By default all the plugins
-  // are attached to #plugins element
-  attachPoint: ".leaflet-popup-pane",
-  attachPointMobile: "#plugins", // for mobile devices
+  className: "plugin-lhpane plugin-mobile-fullscreen",
+  exclusive: "all",
 };
